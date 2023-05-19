c912bb70ebbc0cb053324701c6e2ddf9d602d5b4
AnielQueiroz <anielqsilva@gmail.com>
2023-05-19 16:17:20 -0300

    mudou tudo

--- eliabeBarber/scripts/script.js
+++ eliabeBarber/scripts/script.js
@@ -19,3 +19,31 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
     window.open(whatsappURL);
 });
+
+// carrossel de imagens
+document.addEventListener("DOMContentLoaded", function() {
+    var imageCarousel = new bootstrap.Carousel(document.getElementById('image-carousel'), {
+        interval: 3000,
+        ride: true
+    });
+});
+
+// carousel
+var images = [
+    "../assets/imagem1.jpg",
+    "../assets/imagem2.jpg",
+    "../assets/imagem3.jpg"
+];
+  
+var currentIndex = 0;
+var headerCarousel = document.getElementById("header-carousel");
+  
+function changeHeaderImage() {
+    headerCarousel.style.backgroundImage = "url(" + images[currentIndex] + ")";
+    currentIndex = (currentIndex + 1) % images.length;
+}
+  
+changeHeaderImage();
+setInterval(changeHeaderImage, 9000);
+
+headerCarousel.style.transition = "background-image 0.5s ease-in-out";
