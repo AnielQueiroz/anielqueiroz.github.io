b1c698b82e039a12ca0777b5e5fee0ba29af7bcc
Pedro Barbosa <pedroivo_prb@yahoo.com.br>
2024-08-30 12:34:49 -0300

    feat: added music

---
+++ milady/scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var music = document.getElementById("background-music");
+
+    // Iniciar a música assim que a pagina carrega
+    music.play();
+});
