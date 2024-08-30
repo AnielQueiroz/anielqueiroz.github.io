9202d69118b51092102d29fbbc35080610e75abc
Pedro Barbosa <pedroivo_prb@yahoo.com.br>
2024-08-30 16:17:00 -0300

    feat: added carousel

--- milady/scripts/main.js
+++ milady/scripts/main.js
@@ -5,4 +5,35 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.addEventListener("click", function() {
         music.play();
     }, { once: true }); // Executa o evento apenas uma vez
+
+    // Inicialização do Swiper
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        autoplay: {
+            delay: 5000, // Tempo entre slides em ms
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // Ajusta o comportamento para telas menores, se necessário
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 1,
+            },
+        },
+    });
 });
