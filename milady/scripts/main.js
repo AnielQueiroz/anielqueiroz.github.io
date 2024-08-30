36faeb1963480d350cbab4bdf7a37be47fb32652
Pedro Barbosa <pedroivo_prb@yahoo.com.br>
2024-08-30 12:43:32 -0300

    styles: added music and new fonts

--- milady/scripts/main.js
+++ milady/scripts/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var music = document.getElementById("background-music");
 
-    // Iniciar a música assim que a pagina carrega
-    music.play();
+    // Adicionar um evento de clique para iniciar a música após interação
+    document.body.addEventListener("click", function() {
+        music.play();
+    }, { once: true }); // Executa o evento apenas uma vez
 });
