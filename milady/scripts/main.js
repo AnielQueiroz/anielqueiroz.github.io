2ff2f91563972eee7ecf644e77edb6cc9a1325a9
Aniel <anielqsilva@gmail.com>
2024-09-05 08:28:39 -0300

    style: still working on it

--- milady/scripts/main.js
+++ milady/scripts/main.js
@@ -1,5 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     var music = document.getElementById("background-music");
+    const elements = document.querySelectorAll('.truncate-text');
+
+    elements.forEach(element => {
+        const button = element.nextElementSibling; // O botão "Leia mais" vem logo após o parágrafo
+        if (element.scrollHeight > element.clientHeight) {
+            // Se o conteúdo é maior do que o espaço disponível, mostre o botão
+            button.classList.remove('hidden');
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        // Verifica se o clique foi em um botão com a classe 'leia-mais'
+        if (event.target.classList.contains('leia-mais')) {
+            // Pega o ID do depoimento associado
+            const targetId = event.target.getAttribute('data-target');
+            // Pega o conteúdo completo do depoimento
+            const content = document.getElementById(targetId).innerText;
+            // Define o conteúdo no modal
+            document.getElementById('modal-content').innerText = content;
+            // Mostra o modal
+            document.getElementById('modal').classList.remove('hidden');
+        }
+    });
+    
+    function closeModal() {
+        document.getElementById('modal').classList.add('hidden');
+    }
+    document.getElementById('close-modal').addEventListener('click', closeModal);
 
     // Adicionar um evento de clique para iniciar a música após interação
     document.body.addEventListener("click", function() {
